f31fb179b8fafa0021fb28c270fd45e1afb02fb5
visualstudio <visualstudio@230-PC40>
2019-02-21 16:40:47 -0500

    4:40 COMMIT

--- teamGrape/Program.cs
+++ teamGrape/Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
 
+
 namespace teamGrape
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
+
+
+    class CountrySide
+    {
+        // Create the LinkedList to reflect the Map in the PowerPoint Instructions
+        Village Maeland;
+        Village Helmholtz;
+        Village Alst;
+        Village Wessig;
+        Village Badden;
+        Village Uster;
+        Village Schvenig;
+        public void Travelvillages(Village currentvillage)
+        {
+            if (currentvillage.isAstrildgeHere)
+            {
+                Console.WriteLine("i found dear Astridge in {0}", currentvillage.VillageName);
+                Console.WriteLine("feeling happy");
+                return;
+            }
+            Travelvillages(currentvillage.west);
+            Travelvillages(currentvillage.east);
+        }
+
+        public void Run()
+        {
+            Alst = new Village("Alst", false);
+            Schvenig = new Village("Schveing", false);
+            Wessig = new Village("Wessig", true);
+            Alst.distanceToeastVillage = 14;
+            Alst.distanceTowesttVillage = 19;
+            Alst.west = Schvenig;
+            Alst.east = Wessig;
+
+            Schvenig.west = null;
+            Schvenig.east = null;
+
+            Wessig.west = null;
+            Wessig.east = null;
+        }
+
+
+    }
+
+
+
 }
+
+
+class Village
+{
+    public Village(string _villageName, bool _isAHere)
+    {
+        isAstrildgeHere = _isAHere;
+        VillageName = _villageName;
+    }
+
+    public Village west;
+    public Village east;
+    public string VillageName;
+    public int distanceToeastVillage;
+    public int distanceTowesttVillage;
+    public int distanceToPreviousVillage;
+    public bool isAstrildgeHere;
+}
+
